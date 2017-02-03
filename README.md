815e7a0ef57f27474f9e14aa1992481e031b106a
John Nguyen <jknguye2@illinois.edu>
2017-02-03 12:06:46 -0600

    Finalize framework, add output functionality, tweak slider

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-#  Confidence Elicitation
+# Confidence Elicitation
 
 ## Update log
+
+### 02/03/17
+- Finish the general framework (read questions, process, output answer).
+- Tweak the slider to be more smooth and pass value accurately.
 
 ### 01/24/17
 - Added general framework with jQuery and jQuery-UI.
 - Create general JSON format for question as an example.
 
 ## To Do (john):
+
+### 02/03/17
+- Add the bank functionality.
+- Add front page to select which type study to give out.
+- Fix the text value on the slider to properly reset with new question.
 
 ### 01/24/17
 - Record slider value after submitting question.
--- js/survey.js
+++ js/survey.js
@@ -2,24 +2,31 @@ survey = { questions: undefined,
            firstQuestionDisplayed: -1,
            lastQuestionDisplayed: -1};
 
+////////////////////////////////////////////////////////////////////////////////////
+// Survey Class
+////////////////////////////////////////////////////////////////////////////////////
 (function (survey, $) {
 
     survey.setup_survey = function(questions) {
         var self = this;
+        var slider_value = [];
+
         this.questions = questions;
 
         this.questions.forEach(function(question) {
             self.generateQuestionElement( question );
         });
-      
-        $('#backBtn').click(function() {
-            if ( !$('#backBtn').hasClass('disabled') ) {
-                self.showPreviousQuestionSet();
-            }
-        });
+
       
         $('#nextBtn').click(function() {
             var ok = true;
+
+            // Store the slider value
+            slider_value.push($('#slider').slider('value'));
+
+            // Move the slider back to 0
+            $("#slider").slider("value",  $("#slider").slider("option", "min"));
+
             for (i = self.firstQuestionDisplayed; i <= self.lastQuestionDisplayed; i++) {
                 if (self.questions[i]['required'] === true && !self.getQuestionAnswer(questions[i])) {
                     $('.question-container > div.question:nth-child(' + (i+1) + ') > .required-message').show();
@@ -31,24 +38,33 @@ survey = { questions: undefined,
 
             if ( $('#nextBtn').text().indexOf('Continue') === 0 ) {
                 self.showNextQuestionSet();
+
             }
             else {
+
                 var answers = {res: $(window).width() + "x" + $(window).height()};
                 for (i = 0; i < self.questions.length; i++) {
-                    answers[self.questions[i].id] = self.getQuestionAnswer(self.questions[i]);
+                    answers[self.questions[i].id] = [self.getQuestionAnswer(self.questions[i]), slider_value[i]];
                 }
 
+                // Write answer to file (Note: Only works for Chrome | Not Safari)
+                self.saveAnswers(JSON.stringify(answers), 'solution.json');
+                self.hideAllQuestions();
+                $("#slider").hide();
+                $('#nextBtn').hide();
+                $('.completed-message').text('Thank you for participating in this study! Your answers have been saved.');
+                /*
+                 // USE THIS TO WRITE TO A SERVER
                 $.ajax({type: 'post',
-                        url: 'http://localhost:7000/answers',
+                        url: 'http://localhost:8000',
                         contentType: "application/json",
                         data: JSON.stringify(answers),
                         processData: false,
                         success: function(response) {
                             self.hideAllQuestions();
                             $('#nextBtn').hide();
-                            $('#backBtn').hide();
                             if ('success' in response) {
-                                $('.completed-message').html('Thank you for participating in this survey!<br><br>'+response['success']);
+                                $('.completed-message').html('Thank you for participating in this study!<br><br>'+response['success']);
                             }
                             else if ('error' in response) {
                                 $('.completed-message').text('An error occurred: '+response['error']);
@@ -60,21 +76,27 @@ survey = { questions: undefined,
                         error: function(response) {
                             self.hideAllQuestions();
                             $('#nextBtn').hide();
-                            $('#backBtn').hide();
-                            $('.completed-message').text('An error occurred: could not send data to server');
+                            $('.completed-message').text('An error occurred: could not send data to server!');
                         }
-                });
+                });*/
             }
         });
       
         this.showNextQuestionSet();
-         
+    }
+
+    survey.saveAnswers = function (text, filename){
+        var a = document.createElement('a');
+        a.setAttribute('href', 'data:text/plain;charset=utf-u,' + encodeURIComponent(text));
+        a.setAttribute('download', filename);
+        a.click()
     }
 
     survey.getQuestionAnswer = function(question) {
         var result;
+
         if ( question.type === 'single-select' ) {
-            result = $('input[type="radio"][name="' + question.id + '"]:checked').val();
+            result = $('input[type="radio"][name="' + question.id + '"]:checked').val() ;
         }
         else if ( question.type === 'single-select-oneline' ) {
             result = $('input[type="radio"][name="' + question.id + '"]:checked').val();
@@ -82,12 +104,10 @@ survey = { questions: undefined,
         else if ( question.type === 'text-field-small' ) {
             result = $('input[name=' + question.id + ']').val();
         }
-        else if ( question.type === 'text-field-large' ) {
-            result = $('textarea[name=' + question.id + ']').val();
-        }
         return result ? result : undefined;
     }
 
+    // Display the question
     survey.generateQuestionElement = function(question) {
         var questionElement = $('<div id="' + question.id + '" class="question"></div>');
         var questionTextElement = $('<div class="question-text"></div>');
@@ -105,6 +125,11 @@ survey = { questions: undefined,
                 questionAnswerElement.append('<label class="radio"><input type="radio" value="' + option + '" name="' + question.id + '"/>' + option + '</label>');
             });
         }
+        else if ( question.type === 'text-field-small' ) {
+            // No slider for this
+            questionElement.addClass('text-field-small');
+            questionAnswerElement.append('<input type="text" value="" class="text" name="' + question.id + '">');
+        }
         else if ( question.type === 'single-select-oneline' ) {
             questionElement.addClass('single-select-oneline');
             var html = '<table border="0" cellpadding="5" cellspacing="0"><tr><td></td>';
@@ -118,19 +143,11 @@ survey = { questions: undefined,
             html += '<td><div>' + question.labels[1] + '</div></td></tr></table>';
             questionAnswerElement.append(html);
         }
-        else if ( question.type === 'text-field-small' ) {
-            questionElement.addClass('text-field-small');
-            questionAnswerElement.append('<input type="text" value="" class="text" name="' + question.id + '">');
-        }
-        else if ( question.type === 'text-field-large' ) {
-            questionElement.addClass('text-field-large');
-            questionAnswerElement.append('<textarea rows="8" cols="0" class="text" name="' + question.id + '">');
-        }
         if ( question.required === true ) {
             var last = questionTextElement.find(':last');
             (last.length ? last : questionTextElement).append('<span class="required-asterisk" aria-hidden="true">*</span>');
         }
-        questionAnswerElement.after('<div class="required-message">This is a required question</div>');
+        questionAnswerElement.after('<div class="required-message">Please select an answer before continuing.</div>');
         questionElement.hide();
     }
 
@@ -153,7 +170,7 @@ survey = { questions: undefined,
             if ( this.questions[this.lastQuestionDisplayed]['break_after'] === true)
                 break;
         } while ( this.lastQuestionDisplayed < this.questions.length-1 );
-      
+
         this.doButtonStates();
     }
 
@@ -172,13 +189,6 @@ survey = { questions: undefined,
     }
 
     survey.doButtonStates = function() {
-        if ( this.firstQuestionDisplayed == 0 ) {
-            $('#backBtn').addClass('invisible');  
-        }
-        else if ( $('#backBtn' ).hasClass('invisible') ) {
-            $('#backBtn').removeClass('invisible');
-        }
-        
         if ( this.lastQuestionDisplayed == this.questions.length-1 ) {
             $('#nextBtn').text('Finish');
             $('#nextBtn').addClass('blue');  
@@ -200,3 +210,37 @@ $(document).ready(function(){
 window.onbeforeunload = function() {
     return "This will reset all answers that you've already filled in!";
 }
+
+////////////////////////////////////////////////////////////////////////////////////
+// REGULAR SLIDER
+////////////////////////////////////////////////////////////////////////////////////
+$( function() {
+    var handle = $("#custom-handle");
+
+    $( "#slider" ).slider({
+        range: "min",
+        value: 50,
+        min: 50,
+        max: 100,
+        animate: true,
+        create: function() {
+            setSliderTicks(event.target);
+            handle.text( $( this ).slider( "value" ) );
+        },
+        slide: function( event, ui ) {
+            handle.text( ui.value );
+        }
+    });
+} );
+
+function setSliderTicks(el) {
+    var $slider =  $(el);
+    var max =  $slider.slider("option", "max");
+    var min =  $slider.slider("option", "min");
+    var spacing =  100 / (max - min);
+
+    $slider.find('.ui-slider-tick-mark').remove();
+    for (var i = 0; i < max-min ; i++) {
+        $('<span class="ui-slider-tick-mark"></span>').css('left', (spacing * i) +  '%').appendTo($slider);
+    }
+};
