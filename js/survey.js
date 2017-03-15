99692cfd48e3c76768a4554d7f568ab4a4f43d4f
J. K. Nguyen <jknguye2@illinois.edu>
2017-03-15 01:52:07 -0500

    added parabolic slider to survey.js

--- js/survey.js
+++ js/survey.js
@@ -18,6 +18,7 @@ survey = { questions: undefined,
         var questionCounter = 0; // Keeps track of what question we are currently
         var nextClick = 0; // Count number of time the button next is clicked; used to determine the breaks
         var brier = 0;
+        var answerBank = [];
 
         this.questions = questions;
 
@@ -29,18 +30,20 @@ survey = { questions: undefined,
 
             var subjectID = self.getQuestionAnswer(self.questions[0]);
             var questionID = self.questions[questionCounter]['id'];
-            var group = self.getQuestionAnswer(self.questions[1]);
+            var group =  self.getQuestionAnswer(self.questions[1]);
             var ok = true;
 
+            answerBank.push(self.getQuestionAnswer(self.questions[questionCounter]));
+
             //console.log("question ID:" + self.questions[questionCounter]['id']);
-            console.log("Question Counter:" + questionCounter);
+            //console.log("Question Counter:" + questionCounter);
             //console.log("nextClick value:" + nextClick);
             //console.log("SubjectID: " + self.getQuestionAnswer(self.questions[0]))
-            //console.log("Group: " + self.getQuestionAnswer(self.questions[1]))
-
+            //console.log("Group: " + group)
+            //console.log("QuestioAnswer:" + self.getQuestionAnswer(self.questions[1]))
             //console.log("currentBalance:" + currentBalance);
-            console.log("moneyBank:" + moneyBank);
-
+            //console.log("moneyBank:" + moneyBank);
+            console.log(svg)
             if (questionID == 0 && questionCounter == 1) {
 
                 // Instruction given will depend on the group
@@ -65,14 +68,18 @@ survey = { questions: undefined,
                         "Note, the bank will never go below zero.<br></font>";
                 } else if (group == "Group 3: PS, NB") {
                     document.getElementById('instructions').innerHTML = "<font size=\"4\"><b>INSTRUCTIONS</b>: In this study, you will be asked " +
-                    "\"Yes-No\" type of questions on a variety of topics. For each question, you will also be asked to " +
-                    "quantify your conviction using a parabolic slider as shown below. <br><br> You will receive $10 for your participation, regardless of how " +
-                    "you perform in the experiment. You will have a chance to earn more money based on how many " +
-                    "questions you can answer correctly.<br></font>";
+                        "\"Yes-No\" type of questions on a variety of topics. For each question, you will also be asked to " +
+                        "quantify your conviction using a parabolic slider as shown below. <br><br>" +
+                         "<img src=\"\\image\\pslider.png\" align=\"middle\"> " +
+                         "<br><br>You will receive $10 for your participation, regardless of how " +
+                        "you perform in the experiment. You will have a chance to earn more money based on how many " +
+                        "questions you can answer correctly.<br></font>";
                 } else if (group == "Group 4: PS, B") {
                     document.getElementById('instructions').innerHTML = "<font size=\"4\"><b>INSTRUCTIONS</b>: In this study, you will be asked " +
                         "\"Yes-No\" type of questions on a variety of topics. For each question, you will also be asked to " +
-                        "quantify your conviction using a parabolic slider as shown below. <br><br> You will receive $10 for your participation, regardless of how " +
+                        "quantify your conviction using a parabolic slider as shown below. <br><br> " +
+                        "<img src=\"\\image\\pslider.png\" align=\"middle\"> " +
+                        "<br><br>You will receive $10 for your participation, regardless of how " +
                         "you perform in the experiment. You will have a chance to earn more money based on how many " +
                         "questions you can answer correctly.<br><br> There will be a virtual bank starting at $12 which " +
                         "increases or decreases depending on your answer and your conviction for each question. " +
@@ -80,12 +87,23 @@ survey = { questions: undefined,
                 }
 
             }
-            else if (questionCounter > 1) {
+            else if ((questionCounter > 1)) {
+                group = answerBank[1];
+                if (group == "Group 1: SL, NB" || group == "Group 2: SL, B") {
+                    // Turn on the slider and its label and get rid of instruction
+                    $("#slider").show();
+                    document.getElementById("slider-label").style.display = "inline";
+                    document.getElementById('instructions').innerHTML = "";
+                }
+                else if (group == "Group 3: PS, NB" || group == "Group 4: PS, B"){
+                    // TODO: Call the parabolic slider here
+                    parabolicSlider();
+                    d3.selectAll("svg").attr("visibility", "show");
+                    //d3.selectAll("svg").attr("visibility", "hidden");
+                    document.getElementById("slider-label").style.display = "inline";
+                    document.getElementById('instructions').innerHTML = "";
+                }
 
-                // Turn on the slider and its label and get rid of instruction
-                $("#slider").show();
-                document.getElementById("slider-label").style.display = "inline";
-                document.getElementById('instructions').innerHTML = "";
             }
 
             // Make question required and display message
@@ -102,7 +120,6 @@ survey = { questions: undefined,
 
             // If button is clicked and answer is selected
             if ( $('#nextBtn').text().indexOf('Continue') === 0) {
-
                 // Every n question, show the bank and take a break
                 // TODO: Make continue button appears after 60 seconds
                 if (nextClick % 2 == 0 && nextClick != 0 && questionID != 0) {
@@ -113,6 +130,7 @@ survey = { questions: undefined,
                     // Hide the question and slider
                     self.hideAllQuestions();
                     $("#slider").hide();
+                    d3.selectAll("svg").attr("visibility", "hidden");
                     document.getElementById("slider-label").style.display = "none";
 
                     // Plot the current moneyBank
@@ -162,7 +180,6 @@ survey = { questions: undefined,
                             currentBalance += brier;
                         }
                         moneyBank.push(Number(brier.toFixed(2)));
-
                     }
 
                     // Increase the question index and click counter
@@ -217,6 +234,7 @@ survey = { questions: undefined,
                 self.hideAllQuestions();
                 $("#chart").show();
                 $("#slider").hide();
+                d3.selectAll("svg").attr("visibility", "hidden");
                 $('#nextBtn').hide();
                 document.getElementById("slider-label").style.display = "none";
                 $('.completed-message').text("Thank you for participating in this study! Your answers have been saved.\n You've earned: $" + money.toFixed(2) + "!");
@@ -450,6 +468,7 @@ window.onbeforeunload = function() {
     return "This will reset all answers that you've already filled in!";
 }
 
+
 ////////////////////////////////////////////////////////////////////////////////////
 // REGULAR SLIDER
 ////////////////////////////////////////////////////////////////////////////////////
@@ -472,3 +491,188 @@ $( function() {
     });
 } );
 
+function parabolicSlider() {
+        var data = [];
+
+        // popuate data
+        getData();
+
+        function getData() {
+            for (var i = 0; i < 50; i++) {
+                q = i
+                p = gaussian(q)
+                el = {
+                    "q": q,
+                    "p": p
+                }
+                var div = d3.select("#parabolic").select("#originData")
+                div
+                    .text("x: " + q + ",y: " + p)
+                // .style("left", (d3.event.pageX - 34) + "px")
+                // .style("top", (d3.event.pageY - 12) + "px");
+                //console.log("x: "+q+"y: "+p);
+
+                data.push(el)
+            }
+            ;
+            // need to sort for plotting
+            data.sort(function (x, y) {
+                return x.q - y.q;
+            });
+        }
+
+        function gaussian(x) {
+            return (-1) * x * x;
+        };
+
+
+        // create canvass
+        var margin = {
+                top: 20,
+                right: 20,
+                bottom: 30,
+                left: 50
+            },
+            width = 200 - margin.left - margin.right,
+            height = 500 - margin.top - margin.bottom;
+
+        var svg = d3.select("#parabolic").append("svg")
+            .attr("width", width + margin.left + margin.right)
+            .attr("height", height + margin.top + margin.bottom)
+            .append("g")
+            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// axises
+        var x = d3.scale.linear()
+        // .domain([0,d3.max(data)])
+            .range([0, width]);//can adjust axis range
+
+        var y = d3.scale.linear()
+        // .domain([0,d3.max(data)])
+            .range([height, 0]);
+
+        var xAxis = d3.svg.axis()
+            .scale(x)
+            .orient("top")
+            .tickValues([])
+        var yAxis = d3.svg.axis()
+            .scale(y)
+            .orient("left")
+            .tickValues([])
+
+        x.domain(d3.extent(data, function (d) {
+            return d.q;
+        }));
+        y.domain(d3.extent(data, function (d) {
+            return d.p;
+        }));
+
+        svg.append("g")
+            .attr("class", "x axis")
+            // .attr("transform","translate(10,160)")
+            .call(xAxis);
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis);
+
+// function plot
+        var container = svg.append("g");
+
+        var line = d3.svg.line()
+            .x(function (d) {
+                return x(d.q);
+            })
+            .y(function (d) {
+                return y(d.p);
+            });
+        container.append("path")
+            .datum(data)
+            .attr("class", "line")
+            .attr("d", line);
+
+// handle
+        var drag = d3.behavior.drag()
+            .origin(function (d) {
+                return d;
+            })
+            .on("dragstart", dragstarted)
+            .on("drag", dragged)
+            .on("dragend", dragended);
+
+        handle = [{
+            x: 0,
+            y: 0
+        }];
+
+        handle_circle = container.append("g")
+            .attr("class", "dot")
+            .selectAll('circle')
+            .data(handle)
+            .enter().append("circle")
+            .attr("r", 5)
+            .attr("cx", function (d) {
+                return d.x;
+            })
+            .attr("cy", function (d) {
+                return d.y;
+            })
+            .call(drag);
+
+        function dragstarted(d) {
+            d3.event.sourceEvent.stopPropagation();
+            d3.select(this)
+                .classed("dragging", true);
+        }
+
+        function dragged(d) {
+            d3.select(this)
+                .attr("cx", d.x = d3.event.x)
+                .attr("cy", d.y = (0.025 * d3.event.x * d3.event.x));
+        }
+
+        function dragended(d) {
+            d3.select(this)
+                .classed("dragging", false);
+        }
+
+//color indicator
+        container.append("rect")
+            .attr("x", 0)
+            .attr("y", -30)
+            .attr("width", 0)
+            .attr("height", 20)
+            .attr("fill", "green")
+            .attr("id", "horizontal");
+
+
+        container.append("rect")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", 10)
+            .attr("height", 0)
+            .attr("fill", "red")
+            .attr("id", "vertical");
+
+
+        function findTheMouse() {
+            var coordinates = d3.mouse(this);
+            var div = d3.select("#parabolic").select("#realTime")
+            div
+                .text("x: " + coordinates[0] + ",y: " + coordinates[1])
+                .style("left", (d3.event.pageX - 34) + "px")
+                .style("top", (d3.event.pageY - 12) + "px");
+            svg.select("rect[id='horizontal']")
+                .attr("width", coordinates[0]);
+            svg.select("rect[id='vertical']")
+                .attr("height", coordinates[1]);
+            // .attr("width",xScale)           //pay attention
+            // console.log("x: "+coordinates[0]+"y: "+coordinates[1]);
+            // console.log("y: "+coordinates[1]);
+
+        }
+
+        d3.select("svg")
+            .on("mousemove", findTheMouse);
+
+    parabolicSlider = function(){} // Only allows function to be called once
+};
