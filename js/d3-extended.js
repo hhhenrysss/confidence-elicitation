2e3acda92a7748ed96c89ec0bea1ace92f740cf7
hhhenrysss <songhang2016@hotmail.com>
2018-06-25 14:39:48 -0500

    finish refactoring files

--- js/d3-extended.js
+++ js/d3-extended.js
@@ -168,7 +168,7 @@ if(typeof module === 'object' && module.exports) {
   module.exports = extend;
 }
 else if(typeof define === 'function' && define.amd) {
-  define(['d3'], extend);
+  define(['js/d3'], extend);
 }
 else {
   extend(d3);
