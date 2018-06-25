aa4f67de9c87100c47bbadc58184732b6f1a4926
hhhenrysss <songhang2016@hotmail.com>
2018-06-25 14:38:47 -0500

    file structure refactored

--- js/instructions.js
+++ js/instructions.js
@@ -1,83 +1,170 @@
-var group1_linear_instruction = "<h3><span class='instruction_title'>INSTRUCTIONS</span></h3> \
-<span class='instruction_text'>You will receive $12 for your participation in this experiment. This is yours to keep regardless of how you perform in the experiment.\
-<br><br>\
-You will also have a virtual bank which you will receive its cash value at the end of the experiment. The bank's initial balance is $12.\
-<br><br>\
-By answering each question, your reward or penalty from that question will increase or decrease your bank balance.\
-<br><br>\
-At the end of every 10 questions, you will receive a summary report of your earned reward/penalty in the most recent 10 questions as well as your overall bank balance up to that point of the experiment.\
-<br><br>\
-Your answers will determine your final bank balance. Correct answers will increase your bank balance from the initial $12, and incorrect answers will lower your bank balance, but the bank balance will never go below zero.\
-<br></span>";
+var instructions = (function () {
 
-var group2_parabolic_instruction = "<h3><span class='instruction_title'>INSTRUCTIONS</span></h3>\
- <span class='instruction_text'>You will receive $10 for your participation in this experiment. This is yours to keep regardless of how you perform in the experiment.\
- <br><br>\
- You will also have a virtual bank which you will receive its cash value at the end of the experiment. The bank's initial balance is $12.\
- <br><br>\
- By answering each question, your reward or penalty from that question will increase or decrease your bank balance.\
- <br><br>\
- At the end of every 10 questions, you will receive a summary report of your earned reward/penalty in the most recent 10 questions as well as your overall bank balance up to that point of the experiment.\
- <br><br>\
- Your answers will determine your final bank balance. Correct answers will increase your bank balance from the initial $12, and incorrect answers will lower your bank balance, but the bank balance will never go below zero.\
- <br></span>";
+    var page_home = (function () {
+        function main_prompt () {
+            return 'You must enter a subject ID. If you do not enter an ID and click Continue, the software will reset.';
+        }
 
-var linear_slider_prompts = "\
-<span class='instruction_text'>What is the probability that you think your answer to Part 1 is correct? (Click anywhere on the bar so that the slider appears, then by moving the slider, specify your answer to Part 2)</span>>\
-<br><br>";
+        function main_selection () {
+            return 'Please enter the subject ID.';
+        }
 
-var linear_first_instruction_answer = "<span class='instruction_text'>Based on your knowledge and experience, you may believe it is more likely that Donald Trump was over 40 when he became president, so choosing the \"Yes\" answer is more appropriate.\
-<br><br>\
-Since you have more tendency toward the \"Yes\", you don't stay at 50% and prefer to move the slider to the right of 50%. The stronger is your conviction that your answer is correct, the more you slide to the right.\
-<br><span>";
+        // PUBLIC API
+        return {
+            'main_prompt': main_prompt,
+            'main_selection': main_selection
+        };
+    })();
 
-var linear_second_instruction_answer = "<span class='instruction_text'>Based on your knowledge and experience, you may believe it is less likely that Bill Gates was one of the founders of Apple, so choosing the \“No\" answer is more appropriate.\
-<br><br>\
-Since you have more tendency toward the \“No\", you don't stay at 50% and prefer to move the slider to the right of 50%. The stronger is your conviction that your answer is correct, the more you slide to the right.\
-<br></span>";
+    var page_tutorial = (function () {
+        function tutorial_end_header () {
+            return 'Instructions Ends';
+        }
 
-var linear_third_instruction_answer = "<span class='instruction_text'>Based on your knowledge and experience, you may believe it is more likely that a 5-10 adult is a male than female, so choosing the \"Yes\" answer is more appropriate.\
- <br><br>\
- Since you have more tendency toward the \"Yes\", you don't stay at 50% and prefer to move the slider to the right of 50%. The stronger is your conviction that your answer is correct, the more you slide to the right. But since you realize that there is still some chance that this adult may be female, it is not appropriate to move the slider all the way to 100%, therefore, a slider position somewhere between 50% and 100% is more appropriate.\
- <br></span>";
+        function tutorial_end_explanation () {
+            return [
+                'The three-question tutorial ends here. In subsequent pages, you will encounter questions that have the exactly same format as previous ones.',
+                'Please answer to the best of your knowledge.'
+            ];
+        }
+        function instruction_header () {
+            return 'INSTRUCTIONS';
+        }
+
+        function instruction_text () {
+            return [
+                'You will receive $10 for your participation in this experiment. This is yours to keep regardless of how you perform in the experiment.',
+                'In the subsequent pages you will encounter three sample questions to help you get familiar with the format of questions.'
+            ];
+        }
+        function load_tutorial (type) {
+            if (type === 'parabolic') {
+                return [
+                    {
+                        'question': 'Is the water composed of Hydrogen and Oxygen atoms?',
+                        'answer': 'Based on your knowledge and experience, you may believe that a H2O molecule is composed of two Hydrogen atoms and one Oxygen atom, so choosing the "Yes" answer is more appropriate.',
+                        'graph_instruction': 'Click anywhere on the curve so that the slider appears, then by moving the slider, specify your answer to Part 2. As you move the slider on the curve, the green bar always shows how many points you will earn if your answer is correct. The red bar shows how many points you will lose if your answer is incorrect. Move the slider on the curve to a point where the relative sizes of the green and red bar represents what you are truly willing to gain or lose in case your answer is correct or incorrect. ',
+                        'instruction': 'Since you have some evidence to support the “Yes” answer, it may be acceptable to move the slider all the way to the bottom of the curve. The right place for the slider is somewhere in between where your belief about the likelihood of winning versus losing justifies the length of red and green bars at that point. The more you believe your answer is correct, the more you want to move towards the bottom.'
+                    },
+                    {
+                        'question': 'Does planet Earth orbit around the the Moon?',
+                        'answer': 'Based on your knowledge and experience, you may believe that the Moon orbits around Earth, not the other way around, so choosing the "No" answer is more appropriate.',
+                        'graph_instruction': 'Click anywhere on the curve so that the slider appears, then by moving the slider, specify your answer to Part 2. As you move the slider on the curve, the green bar always shows how many points you will earn if your answer is correct. The red bar shows how many points you will lose if your answer is incorrect. Move the slider on the curve to a point where the relative sizes of the green and red bar represents what you are truly willing to gain or lose in case your answer is correct or incorrect. ',
+                        'instruction': 'Since you have some evidence to support the “No” answer, it may be acceptable to move the slider all the way to the bottom of the curve. The right place for the slider is somewhere in between where your belief about the likelihood of winning versus losing justifies the length of red and green bars at that point. The more you believe your answer is correct, the more you want to move towards the bottom.'
+                    },
+                    {
+                        'question': 'Is it a tail in a random coin toss?',
+                        'answer': 'Based on your knowledge and experience, you may believe that the result depends and may vary each time a coin is tossed.',
+                        'graph_instruction': 'Click anywhere on the curve so that the slider appears, then by moving the slider, specify your answer to Part 2. As you move the slider on the curve, the green bar always shows how many points you will earn if your answer is correct. The red bar shows how many points you will lose if your answer is incorrect. Move the slider on the curve to a point where the relative sizes of the green and red bar represents what you are truly willing to gain or lose in case your answer is correct or incorrect. ',
+                        'instruction': 'Since you are not sure about which side will be at the top after a random coin toss, your answer should lie between the two extremes. The right place for the slider is somewhere in between where your belief about the likelihood of winning versus losing justifies the length of red and green bars at that point. The more you believe your answer is correct, the more you want to move towards the bottom.'
+                    }
+                ];
+            }
+            else if (type === 'linear') {
+                return [
+                    {
+                        'question': 'Is the water composed of Hydrogen and Oxygen atoms?',
+                        'answer': 'Based on your knowledge and experience, you may believe that a H2O molecule is composed of two Hydrogen atoms and one Oxygen atom, so choosing the "Yes" answer is more appropriate.',
+                        'graph_instruction': 'What is the probability that you think your answer to the question is correct? Click anywhere on the bar so that the slider appears, then by moving the slider, specify your confidence level to the question.',
+                        'instruction': 'Since you have more tendency toward the "Yes", you don\'t stay at 50% and prefer to move the slider to the right of 50%. The stronger is your conviction that your answer is correct, the more you slide to the right.'
+                    },
+                    {
+                        'question': 'Does planet Earth orbit around the the Moon?',
+                        'answer': 'Based on your knowledge and experience, you may believe that the Moon orbits around Earth, not the other way around, so choosing the "No" answer is more appropriate.',
+                        'graph_instruction': 'What is the probability that you think your answer to the question is correct? Click anywhere on the bar so that the slider appears, then by moving the slider, specify your confidence level to the question.',
+                        'instruction': 'Since you have more tendency toward the “No", you don\'t stay at 50% and prefer to move the slider to the right of 50%. The stronger is your conviction that your answer is correct, the more you slide to the right.'
+                    },
+                    {
+                        'question': 'Is it a tail in a random coin toss?',
+                        'answer': 'Based on your knowledge and experience, you may believe that the result depends and may vary each time a coin is tossed.',
+                        'graph_instruction': 'What is the probability that you think your answer to the question is correct? Click anywhere on the bar so that the slider appears, then by moving the slider, specify your confidence level to the question.',
+                        'instruction': 'Since you are not sure about which side will be at the top after a random coin toss, your answer should lie between 0% to 100%.'
+                    }
+                ];
+            }
+        }
+
+        // PUBLIC API
+        return {
+            'tutorial_end_header': tutorial_end_header,
+            'tutorial_end_explanation': tutorial_end_explanation,
+            'instruction_header': instruction_header,
+            'instruction_text': instruction_text,
+            'load_tutorial': load_tutorial
+        };
+    })();
+
+    var page_group_selection = (function () {
+        function group_selection_prompt () {
+            return 'You must select a group for the subject. If you do not select a group and click Continue, the software will reset.';
+        }
+
+        function group_selection_question () {
+            return 'What is the subject\'s group?';
+        }
+
+        function group_selection_options () {
+            return {
+                'linear': 'Group 1: Linear Slider',
+                'parabolic': 'Group 2: Parabolic Slider'
+            };
+        }
+
+        // PUBLIC API
+        return {
+            'group_selection_prompt': group_selection_prompt,
+            'group_selection_question': group_selection_question,
+            'group_selection_options': group_selection_options
+        };
+    })();
+
+    var page_end = (function () {
+        function ending_header () {
+            return 'The survey ends here!';
+        }
+        function ending_content () {
+            return "Thank you for your participation in this study! Your answers have been saved.";
+        }
+
+        // PUBLIC API
+        return {
+            'ending_header': ending_header,
+            'ending_content': ending_content
+        };
+    })();
+
+    var page_interval = (function () {
+        function interval_header () {
+            return 'You have answered 10 questions! Time to take a break!';
+        }
+
+        function interval_explanation () {
+            return 'After 1 minute, the "continue" button will appear and you may continue. Please DO NOT switch to another tab or minimize/close the browser.';
+        }
+
+        // PUBLIC API
+        return {
+            'interval_header': interval_header,
+            'interval_explanation': interval_explanation
+        };
+    })();
+
+    function question_selection () {
+        return {
+            'Yes': 'Yes',
+            'No': 'No'
+        };
+    }
 
 
-var parablic_slider_prompts = "\
-<span class='instruction_text'>Click anywhere on the curve so that the slider appears, then by moving the slider, specify your answer to Part 2. As you move the slider on the curve, the green bar always shows how many points you will earn if your answer is correct. The red bar shows how many points you will lose if your answer is incorrect. Move the slider on the curve to a point where the relative sizes of the green and red bar represents what you are truly willing to gain or lose in case your answer is correct or incorrect.\
- <br></span>";
 
-var parabolic_first_question_answer = "<span class='instruction_text'>Based on your knowledge and experience, you may believe it is more likely that Donald Trump was over 40 when he became president, so choosing the \"Yes\" answer is more appropriate.\
-<br><br>\
-Since you have some evidence to support the \“Yes\” answer, it may be acceptable to move the slider all the way to the bottom of the curve.\
-<br><br>\
-The right place for the slider is somewhere in between where your belief about the likelihood of winning versus losing justifies the length of red and green bars at that point. The more you believe your answer is correct, the more you want to move towards the bottom.\
-<br></span>";
-
-
-var parabolic_second_question_answer = "<span class='instruction_text'>Based on your knowledge and experience, you may believe it is less likely that Bill Gates was one of the founders of Apple, so choosing the \“No\" answer is more appropriate. \
-<br><br>\
-Since you have some evidence to support the \“No\” answer, it may be acceptable to move the slider all the way to the bottom of the curve.\
-<br><br>\
-The right place for the slider is somewhere in between where your belief about the likelihood of winning versus losing justifies the length of red and green bars at that point. The more you believe your answer is correct, the more you want to move towards the bottom.\
-<br><br></span>";
-
-var parabolic_third_question_answer = "<span class='instruction_text'>Based on your knowledge and experience, you may believe it is more likely that a 5-10 adult is a male than female, so choosing the \"Yes\" answer is more appropriate.\
-<br><br>\
-Since you have some evidence to support the \“Yes\” answer, it is not acceptable to move the slider all the way to the bottom of the curve. But you are not completely sure that \“Yes\” is the correct answer, so it is not appropriate to move the slider all the way to the top of the curve either.\
-<br><br>\
-The right place for the slider is somewhere in between where your belief about the likelihood of winning versus losing justifies the length of red and green bars at that point. The more you believe your answer is correct, the more you want to move towards the bottom.\
-<br></span>";
-
-
-
-
-var instruction_is_finished = "<span class='instruction_text'>This completes the instructions for your task. If you have any questions, please feel free to share them with the experimenter before proceeding.\
-<br><br>\
-If you have no questions, then please click on the \"Continue\" button to begin performing the experimental task, for which you you will gain or lose money in your bank based on the accuracy of answering the questions and in adjusting the slider knob.\
-<br></span>";
-
-
-var tutorial_starts = "<h3 class='question-text'>Tutorial Begins</h3><br><br>\
-<p>In the following pages there will be three sample questions to help you get familar with the question formats. Your answers not recorded.</p>";
-
-var tutorial_ends = "<h3 class='question-text'>Tutorial Ends Here</h3><br><br>";
+    // PUBLIC API
+    return {
+        'page_interval': page_interval,
+        'page_home': page_home,
+        'page_tutorial': page_tutorial,
+        'page_group_selection': page_group_selection,
+        'page_end': page_end,
+        'question_selection': question_selection
+    };
+})();
